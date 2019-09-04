092698270accaa7f0f65a5ad91ac97f66d26e03f
Ding Li <dingmaotu@hotmail.com>
2019-09-04 21:34:06 +0800

    package: add setup.py for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gsql_client",
+    version="0.1.0",
+    author="Ding Li",
+    author_email="dingmaotu@hotmail.com",
+    description="GSQL and RESTPP Python remote clients for TigerGraph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['tigergraph', 'gsql', 'restpp', 'client'],
+    requires=[],
+    url="https://github.com/dingmaotu/gsql_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Database :: Front-Ends",
+    ],
+    python_requires='>=2.7'
+)
